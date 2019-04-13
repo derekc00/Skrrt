22f8f20ff11ed14b63624922d3f96037e1754c09
derek chang <derekchangc00@gmail.com>
2019-04-13 02:44:51 -0700

    Initial Commit

---
+++ Skrrt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skrrt
+//
+//  Created by Derek Chang on 4/13/19.
+//  Copyright © 2019 Derek Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
